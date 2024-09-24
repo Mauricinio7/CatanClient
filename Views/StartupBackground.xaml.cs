b62c72aaca2f1143a2842827559ead5a8ab28eea
Mauricio Salas Garcia <zS22013639@estudiantes.uv.mx>
2024-09-23 18:21:34 -0600

    The main window and its first user control that controls the background animation have been created.

---
+++ Views/StartupBackground.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAnimatedGif;
+
+namespace CatanClient.Views
+{
+
+    public partial class StartupBackground : UserControl
+    {
+        public StartupBackground()
+        {
+            InitializeComponent();
+            Load_Gif();
+        }
+
+        private void Load_Gif()
+        {
+            var imageUri = new Uri("pack://application:,,,/Resources/Gifs/AnimatedBackground1.gif", UriKind.Absolute);
+            var imageSource = new BitmapImage(imageUri);
+            ImageBehavior.SetAnimatedSource(AnimatedBackground1, imageSource);
+        }
+
+    }
+}
