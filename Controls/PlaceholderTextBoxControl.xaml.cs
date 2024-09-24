869beba520c0293d49fe462d1c527888b0f7640a
Mauricio Salas Garcia <zS22013639@estudiantes.uv.mx>
2024-09-23 18:53:37 -0600

    The login window has been created

---
+++ Controls/PlaceholderTextBoxControl.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CatanClient.Controls
+{
+    /// <summary>
+    /// Lógica de interacción para PlaceholderTextBoxControl.xaml
+    /// </summary>
+    public partial class PlaceholderTextBoxControl : UserControl
+    {
+        public PlaceholderTextBoxControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (PlaceholderText); }
+        }
+
+        public static readonly DependencyProperty PlaceholderTextProperty =
+            DependencyProperty.Register("PlaceholderText", typeof(string), typeof(PlaceholderTextBoxControl), new PropertyMetadata("Escribe aquí..."));
+
+        public string PlaceholderText
+        {
+            get { return (string)GetValue(PlaceholderTextProperty); }
+            set { SetValue(PlaceholderTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderTagProperty =
+            DependencyProperty.Register("PlaceholderTag", typeof(string), typeof(PlaceholderTextBoxControl), new PropertyMetadata(null));
+
+        public string PlaceholderTag
+        {
+            get { return (string)GetValue(PlaceholderTagProperty); }
+            set { SetValue(PlaceholderTagProperty, value); }
+        }
+
+        public static readonly DependencyProperty FontSizeValueProperty =
+            DependencyProperty.Register("FontSizeValue", typeof(double), typeof(PlaceholderTextBoxControl), new PropertyMetadata(15.0));
+
+        public double FontSizeValue
+        {
+            get { return (double)(GetValue(FontSizeValueProperty)); }
+            set { SetValue(FontSizeValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty BackgroundColorValueProperty =
+            DependencyProperty.Register("BackgroundColorValue", typeof(Brush), typeof(PlaceholderTextBoxControl), new PropertyMetadata(Brushes.Black));
+
+        public Brush BackgroundColorValue
+        {
+            get { return (Brush)(GetValue(BackgroundColorValueProperty)); }
+            set { SetValue(BackgroundColorValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderForegroundColorValueProperty =
+            DependencyProperty.Register("ForegroundColorValue", typeof(Brush), typeof(PlaceholderTextBoxControl), new PropertyMetadata(Brushes.LightGray));
+
+        public Brush PlaceholderForegroundColorValue
+        {
+            get { return (Brush)(GetValue(PlaceholderForegroundColorValueProperty)); }
+            set { SetValue(PlaceholderForegroundColorValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty NormalForegroundColorValueProperty =
+            DependencyProperty.Register("NormalForegroundColorValue", typeof(Brush), typeof(PlaceholderTextBoxControl), new PropertyMetadata(Brushes.White));
+
+        public Brush NormalForegroundColorValue
+        {
+            get { return (Brush)(GetValue(NormalForegroundColorValueProperty)); }
+            set { SetValue(NormalForegroundColorValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty PaddingValueProperty =
+            DependencyProperty.Register("PaddingValue", typeof(Thickness), typeof(PlaceholderTextBoxControl), new PropertyMetadata(new Thickness(15)));
+
+        public Thickness PaddingValue
+        {
+            get { return (Thickness)(GetValue(PaddingValueProperty)); }
+            set { SetValue(PaddingValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty BorderThicknessValueProperty =
+            DependencyProperty.Register("BorderThicknessValue", typeof(Thickness), typeof(PlaceholderTextBoxControl), new PropertyMetadata(new Thickness(0)));
+
+        public Thickness BorderThicknessValue
+        {
+            get { return (Thickness)(GetValue(BorderThicknessValueProperty)); }
+            set { SetValue(BorderThicknessValueProperty, value); }
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            string placeholderText = textBox.Tag as string;
+
+            if (textBox.Text == placeholderText)
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = NormalForegroundColorValue;
+            }
+        }
+
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            string placeholderText = textBox.Tag as string;
+
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = placeholderText;
+                textBox.Foreground = PlaceholderForegroundColorValue;
+
+            }
+        }
+    }
+}
---
+++ Views/LoginView.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CatanClient.Views
+{
+    /// <summary>
+    /// Lógica de interacción para LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            string placeholderText = textBox.Tag as string;
+
+            if (textBox.Text == placeholderText)
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = Brushes.White;
+
+                //TODO quit hardcode
+                if (ShowPasswordCheckBox.IsChecked == false && textBox.Tag == PasswordTextBox.Tag)
+                {
+                    textBox.Visibility = Visibility.Collapsed;
+                    PasswordBox.Visibility = Visibility.Visible;
+                    PasswordBox.Focus();
+                }
+
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            string placeholderText = textBox.Tag as string;
+
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = placeholderText;
+                textBox.Foreground = Brushes.LightGray;
+
+            }
+        }
+
+
+        private void ShowPasswordCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(PasswordBox.Password))
+            {
+                PasswordTextBox.Text = PasswordBox.Password;
+                PasswordTextBox.Foreground = Brushes.White;
+            }
+            else
+            {
+                PasswordTextBox.Text = PasswordTextBox.Tag as string;
+                PasswordTextBox.Foreground = Brushes.LightGray;
+            }
+
+            PasswordTextBox.Visibility = Visibility.Visible;
+            PasswordBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowPasswordCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTextBox.Text != PasswordTextBox.Tag as string)
+            {
+                PasswordBox.Password = PasswordTextBox.Text;
+            }
+            else
+            {
+                PasswordBox.Password = string.Empty;
+            }
+
+            if (PasswordTextBox.Text != PasswordTextBox.Tag as string)
+            {
+                PasswordTextBox.Visibility = Visibility.Collapsed;
+                PasswordBox.Visibility = Visibility.Visible;
+            }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxMail.Text != TextBoxMail.PlaceholderTag && !NullOrWhiteSpacePassword() && !string.IsNullOrWhiteSpace(TextBoxMail.Text))
+            {
+                string Password;
+
+                if (ShowPasswordCheckBox.IsChecked == false)
+                {
+                    Password = PasswordBox.Password;
+                }
+                else
+                {
+                    Password = PasswordTextBox.Text;
+                }
+
+                Console.WriteLine("Correo: " + TextBoxMail.Text);
+                Console.WriteLine("Contraseña: " + Password);
+            }
+            else
+            {
+                Console.WriteLine("No se ha insertado un correo o contraseña");
+            }
+        }
+
+        private bool NullOrWhiteSpacePassword()
+        {
+            return string.IsNullOrWhiteSpace(PasswordBox.Password) && (string.IsNullOrWhiteSpace(PasswordTextBox.Text) || PasswordTextBox.Text == PasswordTextBox.Tag as string);
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                PasswordBox.Visibility = Visibility.Collapsed;
+                PasswordTextBox.Text = PasswordTextBox.Tag as string;
+                PasswordTextBox.Foreground = Brushes.LightGray;
+                PasswordTextBox.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
